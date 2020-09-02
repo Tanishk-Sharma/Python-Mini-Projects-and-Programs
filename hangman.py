5698a67661a38845f8c3a25bb582d9aa4c65afdd
Tanishk Sharma <tanishk9597@gmail.com>
2020-09-02 18:41:08 +0530

    Added the class template.
    Actually code is almost ready. But I am wrapping my head around one little bug. Will soon update the code in here too!

---
+++ hangman.py
@@ -0,0 +1,35 @@
+import random
+import re
+
+class Hangman:
+
+    def __init__(self):
+        pass
+
+    def start_new_game(self):
+        pass
+
+    def set_game_parameters(self):
+        pass
+
+    def selecting_word(self):
+        pass
+
+    def print_word(self):
+        pass
+
+    def get_previous_guesses(self):
+        pass
+
+    def check_if_letter_in_selected_word(self, letter):
+        pass
+
+    def add_to_previous_guesses(self, letter):
+        pass
+
+    def did_i_win(self):
+        pass
+
+    def game_over(self):
+        pass
+
