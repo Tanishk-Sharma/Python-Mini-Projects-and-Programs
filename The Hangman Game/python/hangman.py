5ca2e4f67cb37bd02b0b496a32a790da882530f8
Tanishk Sharma <tanishk9597@gmail.com>
2020-10-25 17:21:15 +0530

    Updated with web scraper
    Now using web scraping to fetch words

--- The Hangman Game/python/hangman.py
+++ The Hangman Game/python/hangman.py
@@ -1,83 +1,81 @@
+import requests
+import html2text
+from bs4 import BeautifulSoup
 import random
-from tkinter import *
 import math
+
 
 class Hangman:
 
     def __init__(self):
-        self.word_length_MIN = 4  # lower bound for word length in database
-        self.word_length_MAX = 11  # upper bound for word length in database
-
         self.incorrect_attempts = 0  # Input BEFORE start of new game. Counter Variable for tracking Game Over state
-        self.min_word_length = 0  # Input BEFORE start of new game. Selected word criteria
-
-        self.chosen_word = dict()  # dictionary of distinct letters and their indexes
-        self.revealed_word = list()  # initially a list of '*' characters, will change with correct guesses.
+        self.chosen_word = ''  # dictionary of distinct letters and their indexes
+        self.won = False  # initially a list of '*' characters, will change with correct guesses.
         self.previous_guesses = set()  # With each turn - show all previously guessed letter made by player
-        self.hangman_pics = ['''
+        self.hangman_pics = [r'''
                   +
                   |
                   |
                   |
                   |
                   |
-            =========''', '''
+            =========''', r'''
                ---+
                   |
                   |
                   |
                   |
                   |
-            =========''', '''
+            =========''', r'''
               +---+
                   |
                   |
                   |
                   |
                   |
-            =========''', '''
-              +---+
-              |   |
-                  |
-                  |
-                  |
-                  |
-            =========''', '''
+            =========''', r'''
               +---+
               |   |
-              O   |
                   |
                   |
                   |
-            =========''', '''
+                  |
+            =========''', r'''
               +---+
               |   |
               O   |
+                  |
+                  |
+                  |
+            =========''', r'''
+              +---+
+              |   |
+              O   |
               |   |
                   |
                   |
-            =========''', '''
+            =========''', r'''
               +---+
               |   |
               O   |
              /|   |
                   |
                   |
-            =========''', '''
+            =========''', r'''
               +---+
               |   |
               O   |
              /|\  |
                   |
                   |
-            =========''', '''
+            =========''', r'''
               +---+
               |   |
               O   |
              /|\  |
              /    |
                   |
-            =========''', '''
+            =========''', r'''
               +---+
               |   |
               O   |
@@ -100,29 +98,25 @@ class Hangman:
             self.set_game_parameters()  # Setting the game parameters
             print(line_break)
 
-            print(
-                f'You have chosen:\nIncorrect attempts: {self.incorrect_attempts}\nMinimum word length: {self.min_word_length}')
+            print(f'You have chosen:\nIncorrect attempts: {self.incorrect_attempts}')
 
             proceed = input('Would you like to proceed? [Y/N] ')
 
         print(line_break)
 
         print('Selecting a word ...')
-        self.selecting_word()  # Selecting a word based on parameters entered
-        won = False  # Local variable to track progress of player status and display corresponding message accordingly
-
+        self.get_random_word()  # Selecting a word based on parameters entered
         # From below: each iteration of the loop is a turn
-
-        while self.game_over() is not True:
-
+        ###############################################################################################################
+        while not self.won:
             print(line_break)
 
-            self.print_word()  # Show the word
-
+            self.print_word()
+            if self.won:
+                break
             # Show current status
             print(f'Attempts Remaining: {self.incorrect_attempts}')
             print(f'Previous Guesses: {self.get_previous_guesses()}')
-
             # Show hangman status
             print(self.hangman_pics[self.hangman_pics_jumper])
             letter_choice = input('Choose the next letter: ').strip()  # Guessed letter by player
@@ -136,19 +130,16 @@ class Hangman:
                 go_ahead = True
 
             if go_ahead:
-                self.check_if_letter_in_selected_word(
-                    letter_choice[
-                        0])  # Check if guessed letter is in the selecting word and marking it revealed if found
-                self.add_to_previous_guesses(letter_choice[0])  # Add the guessed letter to the set of previous_guesses
-            won = game.did_i_win()
-            if won:
+                # Check if guessed letter is in the selecting word and marking it revealed if found
+                self.check_if_letter_in_selected_word(letter_choice[0])
+            if self.incorrect_attempts == 0:
                 break
-
         print(line_break)
-        if won:
-            print(f"-->  {self.word}  <--\nYAYY! YOU WON !!")
+
+        if self.won:
+            print(f"-->  {''.join(self.chosen_word)}  <--\nYAYY! YOU WON !!")
         else:
-            print(f"Aww! Game over!\nThe word was: {self.word}")
+            print(f"Aww! Game over!\nThe word was: **{''.join(self.chosen_word)}**")
             print(self.hangman_pics[-1])
         print(line_break)
 
@@ -157,11 +148,8 @@ class Hangman:
         while True:
             try:
                 t1 = int(input("How many incorrect attempts do you want? [1-25]: "))
-                t2 = int(
-                    input(f"What minimum word length do you want? [{self.word_length_MIN}-{self.word_length_MAX}] "))
-                if (t1 in range(1, 26)) and (t2 in range(self.word_length_MIN, self.word_length_MAX + 1)):
+                if t1 in range(1, 26):
                     self.incorrect_attempts = t1
-                    self.min_word_length = t2
                     break
                 else:
                     print('Input is not in the given range. Please try again ...')
@@ -169,33 +157,36 @@ class Hangman:
             except ValueError:
                 print('Invalid Input. Please try again ...')
 
-    def selecting_word(self):
-        """Selects a word from the stored list of words in text files in data directory"""
-        chosen_word_length = random.randrange(self.min_word_length,
-                                              self.word_length_MAX) if self.min_word_length in range(
-            self.word_length_MIN,
-            self.word_length_MAX) else self.word_length_MAX  # Choosing a random length for selecting word
-
-        text_filename_to_open = '../data/words_length_' + str(chosen_word_length) + '.txt'
-        with open(text_filename_to_open, "r") as words_file:
-            words_list = tuple(
-                words_file.read().split(","))  # Reading words from text file and storing each word as a tuple element
-            random_word_position_in_words_list = random.randrange(0, len(
-                words_list))  # Choosing the position of word to select inside fetched tuple of words
-            self.word = words_list[random_word_position_in_words_list]  # Setting the class variable to the chosen word
-        # word is chosen. Now breaking it into dictionary form
-
-        self.chosen_word = dict.fromkeys(set(list(self.word)), 1)
-        for letter in self.chosen_word.keys():
-            matches = re.finditer(letter, self.word)
-            all_indexes = tuple([match.start() for match in matches])
-            self.chosen_word[letter] = all_indexes
-        self.revealed_word = ['*'] * chosen_word_length
+    def get_random_word(self):
+        alphabets = [chr(i) for i in range(97, 123)]
+        url = 'https://learnersdictionary.com/3000-words/alpha/'
+        alpha = random.choice(alphabets)
+        url += alpha
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        words = soup.findAll('ul', class_="a_words")
+        h = html2text.HTML2Text()
+        h.ignore_links = True
+        h.ignore_images = True
+        list_words = h.handle(str(words[0])).split('\n')
+        list_words = [x.replace('* ', '').strip() for x in list_words]
+        list_words = [x[:x.find(' ')] for x in list_words if '(' in x]
+        self.chosen_word = list(random.choice(list_words))
 
     def print_word(self):
         """Displays the word's letters as per the revealed attribute of letter object"""
-        print('              ', end='')
-        print(''.join(self.revealed_word))
+        print('\t\t', end='')
+        count = 0
+        for letter in self.chosen_word:
+            if letter in self.previous_guesses:
+                print(letter, end='')
+                count += 1
+            else:
+                self.won = False
+                print('*', end='')
+        print()
+        if count == len(self.chosen_word):
+            self.won = True
 
     def get_previous_guesses(self):
         """fetches the previous guesses as a space separated string"""
@@ -204,38 +195,25 @@ class Hangman:
 
     def check_if_letter_in_selected_word(self, letter):
         """Marks revealed as True if letter found in selected word"""
-        found = False
-        if letter in self.chosen_word.keys():
-            found = True
-            indexes = self.chosen_word[letter]
-            self.chosen_word.pop(letter, 0)  # removed the letter as it has been guessed now
-            for index in indexes:
-                self.revealed_word[index] = letter
-
-        if not found and letter not in self.previous_guesses:
-            # Using Newton's equations of motion to determine rate of change of jumper
-            acc = 2 *((len(self.hangman_pics)-self.hangman_pics_jumper)/self.incorrect_attempts**2)
-            v = math.sqrt(self.hangman_pics_jumper**2 + (2*acc*len(self.hangman_pics)))
-            self.hangman_pics_jumper = round(v)
-            
-            if self.hangman_pics_jumper >= len(self.hangman_pics)-2:
-                self.hangman_pics_jumper = len(self.hangman_pics) - 2
-            self.incorrect_attempts -= 1
-
-    def add_to_previous_guesses(self, letter):
-        """Adds letter to the set of previous guesses"""
+        # check if letter already guessed. if yes, then do nothing
+        if letter in self.previous_guesses:
+            return
+        # if not guessed earlier, check if letter is in chosen word
+        if letter in self.chosen_word:
+            # if yes, then put it into previous guesses, it will be revealed with checks
+            self.previous_guesses.add(letter)
+            return
+        # if not, add to previous guesses and reduce incorrect attempts
+        # Using Newton's equations of motion to determine rate of change of jumper
         self.previous_guesses.add(letter)
+        
+        acc = 2 * ((len(self.hangman_pics) - self.hangman_pics_jumper) / self.incorrect_attempts ** 2)
+        v = math.sqrt(self.hangman_pics_jumper ** 2 + (2 * acc * len(self.hangman_pics)))
+        self.hangman_pics_jumper = round(v)
 
-    def did_i_win(self):
-        """Determines if game won based on letter revealed or not"""
-        if '*' in self.revealed_word:
-            return False
-        return True
-
-    def game_over(self):
-        """Game is over once incorrect_attempts are exhausted"""
-        if self.incorrect_attempts == 0:
-            return True
+        if self.hangman_pics_jumper >= len(self.hangman_pics) - 2:
+            self.hangman_pics_jumper = len(self.hangman_pics) - 2
+        self.incorrect_attempts -= 1
 
 
 while True:
